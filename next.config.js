2742444b3cceccfe9cb0d24fb74223bcccd1e94f
Vardhan Bora <bsrsai27@gmail.com>
2025-01-05 21:40:25 -0500

    UP

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
+    // swcMinify: true, // Comment this out if causing issues
     images: {
-        domains: ['open.cruip.com','ucarecdn.com', 'www.svgrepo.com','images.unsplash.com', 'res.cloudinary.com']
+      domains: ['open.cruip.com', 'ucarecdn.com', 'www.svgrepo.com', 'images.unsplash.com', 'res.cloudinary.com'],
     },
     webpack(config) {
-        config
-            .module
-            .rules
-            .push({test: /\.svg$/, use: ["@svgr/webpack"]});
-
-        return config;
-    }
-}
-
-module.exports = nextConfig
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
--- src/components/Contact/SocialMedia.tsx
+++ src/components/Contact/SocialMedia.tsx
@@ -4,28 +4,30 @@ import SocialMediaIcon from './SocialMediaIcon';
 export const SocialMediaArray = [
     {
         title: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/vito-medlej-743b1b241/',
+        href: 'https://www.linkedin.com/in/vvbora/',
         color: '#0e76a8',
-        svg: 'https://www.svgrepo.com/show/138936/linkedin.svg'
-
-    }, {
+        svg: 'https://www.svgrepo.com/show/138936/linkedin.svg',
+    },
+    {
         color: '#171515',
         title: 'Github',
-        href: 'https://github.com/VitoMedlej',
-        svg: 'https://www.svgrepo.com/show/343674/github.svg'
-    }, {
+        href: 'https://github.com/VARDHAN-BORA',
+        svg: 'https://www.svgrepo.com/show/343674/github.svg',
+    },
+    {
         title: 'Facebook',
-        href: "https://www.facebook.com/vito.medlej.5/",
+        href: "https://www.facebook.com/vardhanbora/",
         color: '#4267B2',
-        svg: 'https://www.svgrepo.com/show/158427/facebook.svg'
-    }
-]
+        svg: 'https://www.svgrepo.com/show/158427/facebook.svg',
+    },
+];
 
 const SocialMedia = () => {
 
     return (
-        <Box sx={{
-            pt: '1em',
+        <Box
+            sx={{
+                pt: '1em',
             pb:'3em',
             display: 'flex'
         }}>
@@ -39,8 +41,7 @@ const SocialMedia = () => {
                     color={icon.color}/>
             })}
         </Box>
+    );
+};
 
-    )
-}
-
-export default SocialMedia
+export default SocialMedia;
